b888451434e33d9718270ed64c9f3c68cbf05a79
marieooq <otakimarie@gmail.com>
2023-05-12 17:00:17 +0900

    added Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+const Input = () => {
+  return (
+    <input
+      className="border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] "
+      placeholder="you@example.com"
+    />
+  );
+};
+
+export default Input;
