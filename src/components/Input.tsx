be7838bf13b7d76e2e0e336153a9643d747524b3
marieooq <otakimarie@gmail.com>
2023-05-28 12:19:16 +0900

    fixed the width of inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,7 @@ const Input = ({ rounded = "none" }: InputType) => {
   return (
     <input
       className={classNames(
-        "w-96 border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)]",
+        "w-full md:w-80 border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)]",
         { "rounded-none": rounded === "none" },
         { "rounded-md": rounded === "md" },
         { "rounded-full": rounded === "full" }
