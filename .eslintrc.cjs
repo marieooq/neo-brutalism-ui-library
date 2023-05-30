868b858de57b835961dbe303aec672e3fcc80b98
marieooq <otakimarie@gmail.com>
2023-05-30 10:18:53 +0900

    installed Storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
-  },
+    "react-refresh/only-export-components": "warn"
+  }
 };
