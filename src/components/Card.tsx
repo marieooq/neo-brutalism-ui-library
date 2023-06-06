e7d0028ef3b55ec9da432abe3265589732c6df30
marieooq <otakimarie@gmail.com>
2023-06-06 15:31:50 +0900

    changed Card component so it can display props properly

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -30,17 +30,9 @@ const Card = ({
           </figure>
           <div className="px-6 py-5 text-left h-full">
             {date && <p className="text-base mb-4">{date}</p>}
-            {title && <h1 className="text-[32px] mb-4">Neo Brutallism</h1>}
+            {title && <h1 className="text-[32px] font-bold mb-4">{title}</h1>}
             {description && (
-              <p className="text-xs mb-4 line-clamp-4">
-                Neobrutalism is an aesthetic characterized by high contrast
-                elements, bright colors, and bold shapes. It is often used to
-                make a statement, as it is meant to be eye-catching and stand
-                out to the viewer. It is also used in user interface and web
-                design, with features such as vibrant colors and bold shapes.
-                Neobrutalism can also be used in print design, with an example
-                being a bold shape with a vibrant color fill applied to it.
-              </p>
+              <p className="text-xs mb-4 line-clamp-4">{description}</p>
             )}
             {callToActionText && <strong>{callToActionText}</strong>}
           </div>
--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { withRouter } from "storybook-addon-react-router-v6";
 import Card from "../components/Card";
-import SampleImage from "../assets/neo-brutalism-image3.jpg";
+import SampleImage from "../assets/neo-brutalism-image1.jpg";
 
 const meta = {
   title: "Components/Card",
